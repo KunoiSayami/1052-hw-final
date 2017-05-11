4201bf3df0cf42c06db872fa07f88915a0edbf47
Too-Naive <sometimes.naive@hotmail.com>
2017-05-11 17:09:23 +0800

    Finsh French deck store

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,34 @@
  *
  */
 
+import java.util.Random;
+
+class cardstore{
+	static int card[],step,player;
+	static Random rand;
+	//Remove Ghost because 21 point no need
+	public void initCard(){
+		step = 0;
+		boolean tmpsz[] = new boolean[52];
+		for (int i=0,tmp;i<52;i++){
+			while (tmpsz[tmp = rand.nextInt(52)]);
+			card[i] = tmp;
+			tmpsz[tmp] = true;
+		}
+	}
+	public cardstore(int players){
+		card = new int[52];
+		rand = new Random();
+		player = players;
+		this.initCard();
+	}
+	public int getNextPoint(){
+		if (step+player*2>52)
+			this.initCard();
+		return (card[step++]+1%13)>10?10:card[step-1];
+	}
+}
 
 public class Game {
-
+	
 }
