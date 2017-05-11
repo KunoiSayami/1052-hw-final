986ac1281d59dbd73a5a1e1b76958da0ac136472
Too-Naive <sometimes.naive@hotmail.com>
2017-05-11 18:32:29 +0800

    Add player number input

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,13 @@
  */
 
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.Container;
 import java.util.*;
 
 class _cardStore{
@@ -39,6 +46,25 @@ class _cardStore{
 	}
 }
 
-public class Game {
+public class Game extends JFrame{
 	_cardStore cardStore;
+	static int maxplayer = 4;
+	Game(){
+		super("21 point");
+		cardStore = new _cardStore(this.playerchoose());
+	}
+	private int playerchoose(){
+		String playerstr = JOptionPane.showInputDialog("Please input player count");
+		String rematch = "^[1-9]$";
+		Pattern r = Pattern.compile(rematch);
+		Matcher m = r.matcher(playerstr);
+		if (!m.find())
+			JOptionPane.showMessageDialog(null, "Check your input.\n"
+					+ "Error Message: Regular expressions not match("+rematch+")");
+		int players = Integer.parseInt(playerstr);
+		if (players>maxplayer)
+			JOptionPane.showMessageDialog(null, "Check your input.\n"
+					+ "Error Message: Input more than"+maxplayer);
+		return players;
+	}
 }
--- src/GameMain.java
+++ src/GameMain.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 /*
  * This source code was published under GPL v3
  *
@@ -8,5 +10,8 @@
 
 public class GameMain {
 	public static void main(String[] args){
+		Game g = new Game();
+		//g.setVisible(true);
+		g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
