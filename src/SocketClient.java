501d4ff2460db8aacd2b0be9f2762753e85b240c
Too-Naive <sometimes.naive@hotmail.com>
2017-05-13 17:56:36 +0800

    Finish socket client and server

---
+++ src/SocketClient.java
@@ -0,0 +1,49 @@
+/*
+ * This source code was published under GPL v3
+ *
+ * Copyright (C) 2017 Too-Naive
+ *
+ */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class SocketClient {
+	public static void main(String[] args) throws IOException{
+		String host = "";
+		int port = 9487;
+		Socket socket = null;
+		Scanner consoleInput = new Scanner(System.in);
+		host  = consoleInput.nextLine();
+		try{
+			socket = new Socket(host, port);
+			DataInputStream input = null;
+			DataOutputStream output = null;
+			
+			try{
+				input = new DataInputStream(socket.getInputStream());
+				output = new DataOutputStream(socket.getOutputStream());
+				while (true){
+					System.out.println(input.readUTF());
+					break;
+				}
+			}
+			catch (IOException e){}
+			finally {
+				if (input != null) input.close();
+				if (output != null) output.close();
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		finally {
+			if (socket != null) socket.close();
+			if (consoleInput != null) consoleInput.close();
+		}
+	}
+}
---
+++ src/SocketServer.java
@@ -0,0 +1,87 @@
+/*
+ * This source code was published under GPL v3
+ *
+ * Copyright (C) 2017 Too-Naive
+ *
+ */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SocketServer {
+	public static final int LISTEN_PORT = 9487;
+	public void listenRequest(){
+		ServerSocket serverSocket = null;
+		ExecutorService threadExecuteor = Executors.newCachedThreadPool();
+		try{
+			serverSocket = new ServerSocket(LISTEN_PORT);
+			System.out.println("Server listening requests...");
+			while (true){
+				Socket socket = serverSocket.accept();
+				threadExecuteor.execute(new RequestThread(socket));
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		finally{
+			if (threadExecuteor != null)
+				threadExecuteor.shutdown();
+			if (serverSocket != null)
+				try{
+					serverSocket.close();
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+		}
+	}
+	public static void main(String[] args) {
+		SocketServer server = new SocketServer();
+		server.listenRequest();
+	}
+	class RequestThread implements Runnable{
+		private Socket clientSocket;
+		public RequestThread(Socket clientSocket){
+			this.clientSocket = clientSocket;
+		}
+		@Override
+		public void run(){
+			System.out.printf("有%s連線進來\n",clientSocket.getRemoteSocketAddress());
+			DataInputStream input = null;
+			DataOutputStream output = null;
+			try{
+				input = new DataInputStream(this.clientSocket.getInputStream());
+				output = new DataOutputStream(this.clientSocket.getOutputStream());
+				while (true){
+					output.writeUTF(String.format("Hi, %s\n", clientSocket.getRemoteSocketAddress()));
+					output.flush();
+					break;
+				}
+			}
+			catch (IOException e){
+				e.printStackTrace();
+			}
+			finally{
+				try{
+					if (input != null) input.close();
+					if (output != null) output.close();
+					if (this.clientSocket != null && !this.clientSocket.isClosed())
+						this.clientSocket.close();
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
+
+
+
