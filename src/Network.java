d9d4b7a7b1ce65255e72e33cd05cf66502072f14
Too-Naive <sometimes.naive@hotmail.com>
2017-05-15 21:33:29 +0800

    Add logic check

--- src/Network.java
+++ src/Network.java
@@ -15,26 +15,61 @@ public class Network {
 
 class UdpServer extends UdpServerEx{
 	boolean isClientMode;
-	UdpServer(boolean _isClientMode){
+	String[] addressStore;
+	int index;
+	String targetAddr;
+	UdpServer(boolean _isClientMode,int playerCount){
 		super("0.0.0.0");
 		this.isClientMode = _isClientMode;
+		this.addressStore = new String[playerCount];
+		index = 0;
 	}
-	@Override
+
+	@SuppressWarnings("resource")
 	public void run() throws Exception{
 		byte buffer[] = new byte[buffSize];
-		DatagramSocket socket = new DatagramSocket(port, this.inetAddress);
-		while (true){
+		DatagramSocket socket = new DatagramSocket(this.port, this.inetAddress);
+		while (this.check()){
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 			socket.receive(packet);
 			String packmsg = new String(buffer , 0, packet.getLength());
-			System.out.printf("%s:%s\n",packet.getAddress(),packmsg);
+			//System.out.printf("%s:%s\n",packet.getAddress(),packmsg);
+			//return isClientMode?clientProcdata():serverProcdata();
+			
+			if (this.isClientMode)
+				clientProcdata(packet,packmsg);
+			else
+				serverProcdata(packet,packmsg);
 		}
 	}
-	void clientProcdata(){
+
+	boolean check(){
+		return this.isClientMode?true:this.index < this.addressStore.length;
+	}
+	void clientProcdata(DatagramPacket packet,String msg){
 		
 	}
-	void serverProcdata(){
 
+	void serverProcdata(DatagramPacket packet,String msg){
+
+	}
+	
+	String getTarget(){
+		RuntimeException runtimeException = new RuntimeException("targetAddr must be inited");
+		if (this.targetAddr == null)
+			throw runtimeException;
+		return this.targetAddr;
 	}
 }
 
+
+class UdpClient extends UdpClientEx{
+	String msg;
+	public UdpClient(String pServer, int pPort, String mMsg) throws Exception{
+		super(pServer,pPort);
+		this.msg = mMsg;
+	}
+	public void send(){
+		sendEx(this.msg);
+	}
+}
--- src/UdpClientEx.java
+++ src/UdpClientEx.java
@@ -11,30 +11,30 @@ import java.net.*;
 // 2. 本程式必須有一個參數，指定伺服器的 IP。
 // 用法範例： java UdpClient 127.0.0.1
  
-public class UdpClientEx extends Thread {
+public class UdpClientEx {
 	int port;            // port : 連接埠
 	InetAddress server; // InetAddress 是 IP, 此處的 server 指的是伺服器 IP
-	String msg;            // 欲傳送的訊息，每個 UdpClient 只能傳送一個訊息。
+	//String msg;            // 欲傳送的訊息，每個 UdpClient 只能傳送一個訊息。
  
 	public static void main(String args[]) throws Exception {
-		for (int i=0; i<100; i++) {
+	//	for (int i=0; i<100; i++) {
 			// 建立 UdpClient，設定傳送對象與傳送訊息。
-			UdpClientEx client = new UdpClientEx(args[0], 5555, "UdpClient : "+i+"th message");
-			client.run(); // 啟動 UdpClient 開始傳送。
-		}
+		//	UdpClientEx client = new UdpClientEx(args[0], 5555, "UdpClient : "+i+"th message");
+		//	client.run(); // 啟動 UdpClient 開始傳送。
+		//}
 	}
  
-	public UdpClientEx(String pServer, int pPort, String pMsg) throws Exception {
-		port = pPort;                             // 設定連接埠
-		server = InetAddress.getByName(pServer); // 將伺服器網址轉換為 IP。
-		msg = pMsg;                                 // 設定傳送訊息。
+	public UdpClientEx(String pServer, int pPort) throws Exception {
+		this.port = pPort;                             // 設定連接埠
+		this.server = InetAddress.getByName(pServer); // 將伺服器網址轉換為 IP。
+	//	msg = pMsg;                                 // 設定傳送訊息。
 	}
  
-	public void run() {
+	public void sendEx(String msg) {
 	  try {
 		byte buffer[] = msg.getBytes();                 // 將訊息字串 msg 轉換為位元串。
 		// 封裝該位元串成為封包 DatagramPacket，同時指定傳送對象。
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server, port); 
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, this.server, this.port); 
 		DatagramSocket socket = new DatagramSocket();    // 建立傳送的 UDP Socket。
 		socket.send(packet);                             // 傳送
 		socket.close();                                 // 關閉 UDP socket.
