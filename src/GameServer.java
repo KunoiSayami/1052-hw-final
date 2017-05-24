a7571976b4ed10d9dabf476fa6cb60234b27acd6
Too-Naive <sometimes.naive@hotmail.com>
2017-05-24 16:00:03 +0800

    Move online user check to class

--- src/GameServer.java
+++ src/GameServer.java
@@ -8,26 +8,33 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import java.util.Arrays;
+import java.util.Random;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 class GameServer extends NetworkFather{
 	int playerCount;
-	int onlinePlayer;
 	String[] clientAddressStrings;
-	String[] onlineAddressStrings;
 	ReentrantLock threadLock;
 	String[] syncStrings;
-	CardStore[] cardStores;
+	Hand[] hand;
+	static OnlineUser onlineUser;
+	static CardStore cardStore = null;
 	GameServer(int _playerCount){
 		this.playerCount = _playerCount;
 		this.clientAddressStrings = new String[this.playerCount];
 		this.threadLock = new ReentrantLock();
 		this.syncStrings = new String[this.playerCount];
-		this.cardStores = new CardStore[this.playerCount];
+		this.hand = new Hand[this.playerCount];
+		if (cardStore == null)
+			cardStore = new CardStore(this.playerCount);
 	}
 	public void listenRequest(){
 		ServerSocket serverSocket = null;
@@ -37,7 +44,7 @@ class GameServer extends NetworkFather{
 			while (true){
 				Socket socket = serverSocket.accept();
 				threadExecuteor.execute(new RequestThread(
-					socket,this.onlineAddressStrings,this.threadLock
+					socket,onlineUser,this.threadLock
 					));
 			}
 		}
@@ -77,20 +84,24 @@ class GameServer extends NetworkFather{
 
 	class RequestThread implements Runnable{
 		private Socket clientSocket;
-		private String [] clientOnlineStrings;
+		//private String [] clientOnlineStrings;
+		private OnlineUser onlineUser;
 		private int clientID;
 		ReentrantLock threadLock;
-		public RequestThread(Socket _clientSocket , String [] _clientOnlineStrings,ReentrantLock _threadLock)
+		public RequestThread(Socket _clientSocket,
+			OnlineUser _onlineUser,
+			ReentrantLock _threadLock)
 			throws IndexOutOfBoundsException {
-			this.clientID = 0;
-			this.clientSocket = _clientSocket;
-			this.clientOnlineStrings = _clientOnlineStrings;
-			this.threadLock = _threadLock;
-			this.checkAvaliable();
+				this.clientID = 0;
+				this.clientSocket = _clientSocket;
+				//this.clientOnlineStrings = _clientOnlineStrings;
+				this.onlineUser = _onlineUser;
+				this.threadLock = _threadLock;
+				this.checkAvaliable();
 		}
 		void checkAvaliable() throws IndexOutOfBoundsException{
-			for (int i = 0 ; i < this.clientOnlineStrings.length; i++)
-				if (this.clientSocket.getInetAddress().getHostAddress() == this.clientOnlineStrings[i]){
+			for (int i = 0 ; i < this.onlineUser.getOnlineUserStrings().length; i++)
+				if (this.clientSocket.getInetAddress().getHostAddress() == this.onlineUser.getOnlineUserStrings()[i]){
 					this.clientID = i;
 					break;
 				}
@@ -131,9 +142,9 @@ class GameServer extends NetworkFather{
 }
 
 
-class CardStore{
+class Hand{
 	int point;
-	CardStore(){
+	Hand(){
 		this.point = 0;
 	}
 	public boolean addPoint(int _point){
@@ -146,3 +157,71 @@ class CardStore{
 	}
 }
 
+
+
+class CardStore{
+	static int card[] = null,step,player;
+	static Random rand;
+	//Remove Ghost because 21 point no need
+	private void initCard(){
+		step = 0;
+		boolean tmpsz[] = new boolean[52];
+		for (int i=0,tmp;i<52;i++){
+			while (tmpsz[tmp = rand.nextInt(52)]);
+			card[i] = tmp;
+			tmpsz[tmp] = true;
+		}
+	}
+	public CardStore(int players){
+		// https://goo.gl/XVQGpD
+		if (card != null)
+			return ;
+		card = new int[52];
+		rand = new Random();
+		player = players;
+		this.initCard();
+	}
+	public int getNextPoint(){
+		if (step+player*3+4>52)
+			this.initCard();
+		return ((card[step++]+1)%13)>10?10:card[step-1];
+	}
+	public void Debug_showCardSz(){
+		Arrays.sort(card);
+		for (int i=0;i<52;i++)
+			System.out.printf("%d ",card[i]);
+	}
+}
+
+class OnlineUser{
+	int onlineUserCount;
+	String[] onlineUserStrings;
+	int playerLimit;
+	OnlineUser(int totalPlayer){
+		this.onlineUserCount = 0;
+		this.playerLimit = totalPlayer;
+		this.onlineUserStrings = new String[this.playerLimit];
+	}
+	public void write(String remoteAddress){
+		boolean is_repeat = false;
+		for (String x:this.onlineUserStrings)
+			if (remoteAddress == x){
+				is_repeat = true;
+				break;
+			}
+		if (is_repeat)
+			return ;
+		onlineUserStrings[onlineUserCount] = remoteAddress;
+		onlineUserCount++;
+	}
+	public void write(Socket remoteSocket){
+		this.write(remoteSocket.getInetAddress().getHostAddress());
+	}
+	public int getOnlineUserCount() {
+		return onlineUserCount;
+	}
+	public String[] getOnlineUserStrings() {
+		return onlineUserStrings;
+	}
+
+}
--- src/Network.java
+++ src/Network.java
@@ -7,7 +7,6 @@
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Socket;
 
 public class Network {
 	
